663e2d178953dbf4e0b1e85dfecc059f0bfe4682
vikashtiwary118 <tiwaryvikash118@gmail.com>
2019-10-06 08:49:32 +0530

    add project

---
+++ sessionproject2/testapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from testapp.forms import NameForm,AgeForm,GfForm
+# Create your views here.
+
+def name_view(request):
+	form=NameForm()
+	return render(request,'testapp/name.html',{'form':form})
+
+def age_view(request):
+	name=request.GET['name']
+	request.session['name']=name
+	form=AgeForm()
+	return render(request,'testapp/age.html',{'form':form})
+
+def gf_view(request):
+	age=request.GET['age']
+	request.session['age']=age
+	form=GfForm()
+	return render(request,'testapp/gf.html',{'form':form})
+
+def result_view(request):
+	gf=request.GET['gf']
+	request.session['gf']=gf
+	return render(request,'testapp/results.html')
+
